dbd7a00730ec51062c4d18a5eb8eadde0ac095e4
wyc316 <wyc3146@gmail.com>
2016-12-20 20:33:17 +0800

    项目初始化

---
+++ src/main/java/com/wyc/tips/FirstMain.java
@@ -0,0 +1,12 @@
+package com.wyc.tips;/**
+ * Created by wyc on 2016/12/19.
+ */
+
+/**
+ * @author wangyongcan
+ */
+public class FirstMain {
+    public static void main(String args[]) {
+        System.out.println("run success!!!");
+    }
+}
